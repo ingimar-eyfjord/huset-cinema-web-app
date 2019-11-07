bb5b5940833487c68f0c1d92861a8488ef12674f
Ingimar <ingimareys93@gmail.com>
2019-11-07 15:56:41 +0100

    Both modals working.

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ const eventdate = postCopy.querySelector(".showdate");
 	
 const price = postCopy.querySelector(".price");
 	price.textContent=post.price;
-
-
+	
 	
 // URL parameter
 	var a = postCopy.querySelector(".seemore"); 
@@ -34,6 +33,72 @@ const price = postCopy.querySelector(".price");
 	
 document.querySelector("#post").appendChild(postCopy)
 }
+
+
+//modal
+window.addEventListener("DOMContentLoaded", seegenre);
+function seegenre(){
+	fetch("http://iesdesigner.eu/wordpress/wp-json/wp/v2/genre")
+	.then(res=>res.json())
+	.then(handlemodalData)}
+function handlemodalData(myData){
+	myData.forEach(showgenre)
+	console.log(myData)
+}
+function showgenre(genre){
+const modal = document.querySelector(".genreModal").content;
+//const jsonmodalinfo = modal.cloneNode(true);
+	const genreModal = document.querySelector(".genreModal");
+	genreModal.innerHTML += `<h3 class="genrename">${genre.name}</h3>`;
+	
+document.querySelector(".Genrefilter").addEventListener("click", seetrailer);
+
+  function seetrailer(myData) {
+    const genremodal = document.querySelector(".modal-background");
+
+    //...
+    genremodal.classList.remove("hide");
+  }
+
+	//close the modal when clicked
+  const genremodal = document.querySelector(".modal-background");
+  genremodal.addEventListener("click", () => {
+    genremodal.classList.add("hide");
+  });
+}
+
+//modal
+window.addEventListener("DOMContentLoaded", seevenue);
+function seevenue(){
+	fetch("http://iesdesigner.eu/wordpress/wp-json/wp/v2/venues")
+	.then(res=>res.json())
+	.then(handlevenuelData)}
+function handlevenuelData(venueData){
+	venueData.forEach(showvenue)
+	console.log(venueData)
+}
+function showvenue(venue){
+const modal = document.querySelector(".venueModal").content;
+//const jsonmodalinfo = modal.cloneNode(true);
+	const genreVenue = document.querySelector(".venueModal");
+	genreVenue.innerHTML += `<h3 class="venuename">${venue.name}</h3>`;
+	
+document.querySelector(".Venuefilter").addEventListener("click", showvenuemodal);
+
+  function showvenuemodal(venueData) {
+    const venuemodal = document.querySelector(".modal-background-venue");
+
+    //...
+    venuemodal.classList.remove("hide");
+  }
+
+	//close the modal when clicked
+  const genremodal = document.querySelector(".modal-background-venue");
+  genremodal.addEventListener("click", () => {
+    genremodal.classList.add("hide");
+  });
+}
+
 
 function myFunction() {
     var input, filter, li, a, i, txtValue;
